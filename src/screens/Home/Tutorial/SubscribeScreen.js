1e43ec9695ce77ccdb3fd0247716e5999013d513
Destinyqjy <89529037+Destinyqjy@users.noreply.github.com>
2023-10-14 15:53:10 +0800

    Add files via upload

---
+++ src/screens/Home/Tutorial/SubscribeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SubscribeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#E7F6FF' }}>
+      <Text>SubscribeScreen</Text>
+      <Text>更多功能开发中...</Text>
+    </View>
+  )
+}
