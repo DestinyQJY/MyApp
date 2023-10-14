41b0f992e860a40a8bcef8af462b946d860302f9
Destinyqjy <89529037+Destinyqjy@users.noreply.github.com>
2023-10-14 15:49:17 +0800

    Create RecommendScreen.js

---
+++ src/navigation/screens/Home/Tutorial/RecommendScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RecommendScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#E7F6FF' }}>
+      <Text>RecommendScreen</Text>
+      <Text>更多功能开发中...</Text>
+    </View>
+  )
+}
