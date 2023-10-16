12d174b0397706087df5b19f33327b858852fb6c
Destinyqjy <89529037+Destinyqjy@users.noreply.github.com>
2023-10-16 18:12:22 +0800

    Update severConfig.js

--- src/config/severConfig.js
+++ src/config/severConfig.js
@@ -1,12 +1,25 @@
+// 服务器配置 
 const severConfig = {
     host: '152.136.45.88',
     port: 5000,
 }
 
-const getProductDataApi = () => (
+// 用户登录
+const postUserLoginUrl = (userid, password) => (
+    `http://${severConfig.host}:${severConfig.port}/api/user/login?userid=${userid}&password=${password}`
+)
+
+// 用户注册
+const postUserRegisterUrl = () => (
+    `http://${severConfig.host}:${severConfig.port}/api/user/register`
+)
+
+// 获取产品库信息
+const getProductDataUrl = () => (
     `http://${severConfig.host}:${severConfig.port}/api/products`
 )
 
-export { getProductDataApi }
+
+export { postUserLoginUrl, postUserRegisterUrl, getProductDataUrl }
 
 export default severConfig
